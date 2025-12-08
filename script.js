f95741e60674b36767200f4cf922fe6df8762b2e
guesswhojustme <imnayeonsaranghaee@gmail.com>
2025-12-08 18:38:20 +0800

    refactor: removed some unecessary codes

--- script.js
+++ script.js
@@ -1,79 +1,25 @@
-// const playBtn = document.getElementById('play')
-
-// const game = (function () {
-
-//     const gameBoard = {
-//         array: [
-//     ['*','*','*'],
-//     ['*','*','*'],
-//     ['*','*','*']
-// ]
-//     };
-
-    
-//     const gameObj = {
-//         board: [],
-//         player1: 'x',
-//         player2: '0'
-//     }
-
-
-// })();
-
-let turns = 0;
-
 const array = [
     ['o','o','x'],
-    ['x','o','x'],
-    ['x','x','x']
+    ['o','o','x'],
+    ['x','x','o']
 ]
 
-let gameStatus = "continue";
-
-function player(name, marker){
-    return {
-        marker, name
-    };
-};
-
-const player1 = player('Marc', 'X');
-const player2 = player('Josh', 'O');
-
-console.log(player1);
-console.log(player2);
-
-function displayArray(array){
-    for(let i = 0; i < array.length; i++){
-        console.log(array[i]);
-    }
-}
-
-console.log(displayArray(array));
-
-
-function checkArray(array){
-    for(let i = 0; i < array.length; i++){
-        for(let j = 0; j < array[i].length + 1; j++){
-            if(array[i][j] === '*'){
-                 let row = +prompt("row starting with 0: ")
-                    console.log(row);
-
-                    let column = +prompt("column starting with 0: ")
-                    console.log(column);
-
-                    array[row][column] = player1.marker
-                    displayArray(array);
-            }else{
-                gameStatus = "stop"
-            }
-        }
-    }
-}
-
-let oMarkerCounter = 0;
-let xMarkerCounter = 0;
+let turns = 0;
 let xWinner = false;
 let oWinner = false;
+
+const counter = (function () {
+    let oMarkerCounter = 0;
+    let xMarkerCounter = 0;
+
+    return {
+        xIncreament() {xMarkerCounter++},
+        oIncreament() {oMarkerCounter++},
+        resetCounter() {xMarkerCounter = 0; oMarkerCounter = 0;},
+        getMarkerOVal() { return oMarkerCounter},
+        getMarkerXVal() { return xMarkerCounter}
+    }
+})();
 
 
 const arrayDirections = {
@@ -97,101 +43,90 @@ function checkHorizontal(array){
     for(i = 0; i < array.horizontal.length; i++){
         for(j = 0; j < array.horizontal[i].length; j++){
             if(array.horizontal[i][j] == 'x'){
-                xMarkerCounter++
-                if(xMarkerCounter == array.horizontal[i].length){
+                counter.xIncreament();
+                if(counter.getMarkerXVal() == array.horizontal[i].length){
                     xWinner = true;
                 }
             }
             if(array.horizontal[i][j] == 'o'){
-                oMarkerCounter++
-                if(oMarkerCounter == array.horizontal[i].length){
+                counter.oIncreament();
+                if(counter.getMarkerOVal() == array.horizontal[i].length){
                     oWinner = true;
                 }
             }
         }
-        if(xMarkerCounter !== array.horizontal[i].length || oMarkerCounter !== array.horizontal[i].length){
-            xMarkerCounter = 0;
-            oMarkerCounter = 0;
+        if(counter.getMarkerXVal() !== array.horizontal[i].length || counter.getMarkerOVal() !== array.horizontal[i].length){
+            counter.resetCounter();
         }
-        console.log(xMarkerCounter);
-        console.log(oMarkerCounter);
+        console.log(counter.getMarkerOVal());
+        console.log(counter.getMarkerXVal());
     }
     if(xWinner){
         console.log("X won the Game");
-        console.log("checkHorizontal function was satisfied");
     }else if(oWinner){
         console.log("O won the Game");
-        console.log("checkHorizontal function was satisfied");
     }
 };
-
-checkHorizontal(arrayDirections);
 
 function checkDiagonal(array){
     for(i = 0; i < array.diagonal.length; i++){
         for(j = 0; j < array.diagonal[i].length; j++){
             if(array.diagonal[i][j] == 'x'){
-                xMarkerCounter++
-                if(xMarkerCounter == array.diagonal[i].length){
+                counter.xIncreament();
+                if(counter.getMarkerXVal()== array.diagonal[i].length){
                     xWinner = true;
                 }
             }
             if(array.diagonal[i][j] == 'o'){
-                oMarkerCounter++
-                if(oMarkerCounter == array.diagonal[i].length){
+                counter.oIncreament();
+                if(counter.getMarkerOVal() == array.diagonal[i].length){
                     oWinner = true;
                 }
             }
         }
-        if(xMarkerCounter !== array.diagonal[i].length || oMarkerCounter !== array.diagonal[i].length){
-            xMarkerCounter = 0;
-            oMarkerCounter = 0;
+        if(counter.getMarkerXVal() !== array.diagonal[i].length || counter.getMarkerOVal() !== array.diagonal[i].length){
+            counter.resetCounter();
         }
-        console.log(xMarkerCounter);
-        console.log(oMarkerCounter);
+        console.log(counter.getMarkerOVal());
+        console.log(counter.getMarkerXVal());
     }
     if(xWinner){
         console.log("X won the Game");
-        console.log("checkDiagonal function was satisfied");
     }else if(oWinner){
         console.log("O won the Game");
-        console.log("checkDiagonal function was satisfied");
     }
 };
-
-checkDiagonal(arrayDirections);
-
 
 function checkVertical(array){
     for(i = 0; i < array.vertical.length; i++){
         for(j = 0; j < array.vertical[i].length; j++){
             if(array.vertical[i][j] == 'x'){
-                xMarkerCounter++
-                if(xMarkerCounter == array.vertical[i].length){
+                counter.xIncreament();
+                if(counter.getMarkerXVal() == array.vertical[i].length){
                     xWinner = true;
                 }
             }
             if(array.vertical[i][j] == 'o'){
-                oMarkerCounter++
-                if(oMarkerCounter == array.vertical[i].length){
+                counter.oIncreament();
+                if(counter.getMarkerOVal() == array.vertical[i].length){
                     oWinner = true;
                 }
             }
         }
-        if(xMarkerCounter !== array.vertical[i].length || oMarkerCounter !== array.vertical[i].length){
-            xMarkerCounter = 0;
-            oMarkerCounter = 0;
+        if(counter.getMarkerXVal() !== array.vertical[i].length || counter.getMarkerOVal() !== array.vertical[i].length){
+            counter.resetCounter();
         }
-        console.log(xMarkerCounter);
-        console.log(oMarkerCounter);
+        console.log(counter.getMarkerOVal());
+        console.log(counter.getMarkerXVal());
     }
     if(xWinner){
         console.log("X won the Game");
-        console.log("checkVertical function was satisfied");
     }else if(oWinner){
         console.log("O won the Game");
-        console.log("checkVertical function was satisfied");
     }
 };
 
+
+checkDiagonal(arrayDirections);
 checkVertical(arrayDirections);
+checkHorizontal(arrayDirections);
